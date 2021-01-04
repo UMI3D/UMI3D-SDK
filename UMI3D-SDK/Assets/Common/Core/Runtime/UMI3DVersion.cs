c7d2a663e49af7246d918400e8d99d639abb22f9
Theo-Desbonnets <theo.desbonnets@gfi.fr>
2021-01-04 09:25:28 +0100

    Update ReadMe and UMI3DVersion

--- UMI3D-SDK/Assets/Common/Core/Runtime/UMI3DVersion.cs
+++ UMI3D-SDK/Assets/Common/Core/Runtime/UMI3DVersion.cs
@@ -22,7 +22,7 @@ namespace umi3d
         public readonly static string major = "2";
         public readonly static string minor = "1";
         public readonly static string status = "b";
-        public readonly static string date = "201229";
+        public readonly static string date = "210104";
 
     }
 }
