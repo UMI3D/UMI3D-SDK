a5c6ebd689403d721160a0df86dde8d81071f3d9
Theo-Desbonnets <theo.desbonnets@gfi.fr>
2021-01-06 15:40:24 +0100

    Escape character should be handle by the sdk

--- UMI3D-SDK/Assets/ClientDevelopmentKit/Core/Runtime/Loaders/ObjMeshDtoLoader.cs
+++ UMI3D-SDK/Assets/ClientDevelopmentKit/Core/Runtime/Loaders/ObjMeshDtoLoader.cs
@@ -85,6 +85,7 @@ namespace umi3d.cdk
                 localEulerAngles = UMI3DResourcesManager.Instance.transform.eulerAngles + rotOffset,
                 localScale = UMI3DResourcesManager.Instance.transform.lossyScale,
                 authorization = authorization,
+                authorizationName = common.UMI3DNetworkingKeys.Authorization,
                 zUp = false,
                 hideWhileLoading = true,
 
