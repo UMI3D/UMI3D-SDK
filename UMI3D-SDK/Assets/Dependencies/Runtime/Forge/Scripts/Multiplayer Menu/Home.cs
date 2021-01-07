a8d8a080a25c7358a41228d0f87d2acb0ff14dce
Theo-Desbonnets <theo.desbonnets@gfi.fr>
2021-01-07 19:25:36 +0100

    Revert "Remove a lot of useless file in Forge dependencies and add license"
    This reverts commit 82d88bb55dc4cb9955c760ed2f502fcd42d5e360.

---
+++ UMI3D-SDK/Assets/Dependencies/Runtime/Forge/Scripts/Multiplayer Menu/Home.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Home : MonoBehaviour {
+
+    public void LoadScene(int number)
+    {
+        SceneManager.LoadScene(number);
+    }
+
+}
