930af07a4eea49ec4a1c1ef209f1edef4f230b8e
Quentin-Tran <quentin.tran@gfi.fr>
2021-04-23 11:43:39 +0200

    Update version to 2.3.b.210423

--- UMI3D-SDK/Assets/Common/Core/Runtime/UMI3DVersion.cs
+++ UMI3D-SDK/Assets/Common/Core/Runtime/UMI3DVersion.cs
@@ -20,9 +20,9 @@ namespace umi3d
     {
         public static string version { get { return major + "." + minor + "." + status + "." + date; } }
         public readonly static string major = "2";
-        public readonly static string minor = "2";
-        public readonly static string status = "r";
-        public readonly static string date = "210402";
+        public readonly static string minor = "3";
+        public readonly static string status = "b";
+        public readonly static string date = "210423";
 
     }
 }
