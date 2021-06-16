a8c6d6081da962a49517b20d0408e610956f7107
Nicolas Pacqueriaud <nicolas.pacqueriaud@gfi.fr>
2021-06-16 09:15:16 +0200

    Fixing Kalman User Binding for CollaborativeUserAvatar

--- UMI3D-SDK/Assets/ClientDevelopmentKit/Collaboration/Runtime/TrackingKalmanFilters/UMI3DCollaborativeUserAvatar.cs
+++ UMI3D-SDK/Assets/ClientDevelopmentKit/Collaboration/Runtime/TrackingKalmanFilters/UMI3DCollaborativeUserAvatar.cs
@@ -51,7 +51,7 @@ namespace umi3d.cdk.collaboration
                     if (boneBindingDto.active && savedTransforms.ContainsKey(new BoundObject() { objectId = boneBindingDto.objectId, rigname = boneBindingDto.rigName }))
                     {
                         SavedTransform st = savedTransforms[new BoundObject() { objectId = boneBindingDto.objectId, rigname = boneBindingDto.rigName }];
-                        st.obj.position = boneTransform.position + (Vector3)boneBindingDto.offsetPosition;
+                        st.obj.position = boneTransform.position + boneTransform.TransformDirection((Vector3)boneBindingDto.offsetPosition);
                         st.obj.rotation = boneTransform.rotation * (Quaternion)boneBindingDto.offsetRotation;
                     }
                 }
