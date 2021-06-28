5685f3a36809b81f1f6783155e3f1a8955b6ccee
Guillaume Gamelin <guillaume.gamelin@gfi.fr>
2021-06-28 15:18:55 +0200

    added cylinders

--- UMI3D-SDK/Assets/Common/Core/Runtime/Dto/UMI3DContent/Volumes/GeometryTools.cs
+++ UMI3D-SDK/Assets/Common/Core/Runtime/Dto/UMI3DContent/Volumes/GeometryTools.cs
@@ -637,12 +637,6 @@ namespace umi3d.common.volume
             return surfaceNormal.normalized;
         }
 
-
-
-
-
-
-
         /// <summary>
         /// Get the faces of the base of a volume. 
         /// </summary>
@@ -752,7 +746,57 @@ namespace umi3d.common.volume
             return baseSurface;
         }
 
+        public static Mesh GetCylinder(Vector3 position, Quaternion rotation, Vector3 scale, float radius, float height, int subdiv = 16)
+        {
+            Mesh mesh = new Mesh();
 
+            List<Vector3> vertices = new List<Vector3>();
+            List<int> faces = new List<int>();
+
+            for (int i = 0; i < subdiv; i++)
+            {
+                vertices.Add(position + Vector3.Scale(scale, rotation * Quaternion.Euler(i * 360f / subdiv * Vector3.up) * Vector3.right * radius));
+            }
+            for (int i = 0; i < subdiv; i++)
+            {
+                vertices.Add(position + Vector3.Scale(scale, rotation * (Quaternion.Euler(i * 360f / subdiv * Vector3.up) * Vector3.right * radius + height * Vector3.up)));
+            }
+
+            for(int i = 0; i < subdiv - 1; i++)
+            {
+                faces.Add(i);
+                faces.Add(i + 1);
+                faces.Add(i + subdiv);
+
+                faces.Add(i + 1);
+                faces.Add(i + subdiv + 1);
+                faces.Add(i + subdiv);
+
+                faces.Add(subdiv - 1);
+                faces.Add(i + 1);
+                faces.Add(i);
+
+                faces.Add(2 * subdiv - 1);
+                faces.Add(subdiv + i);
+                faces.Add(subdiv + i + 1);
+            }
+
+            faces.Add(subdiv - 1);
+            faces.Add(0);
+            faces.Add(subdiv + subdiv - 1);
+
+            faces.Add(0);
+            faces.Add(subdiv);
+            faces.Add(subdiv - 1 + subdiv);
+
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = faces.ToArray();
+            mesh.RecalculateNormals();
+            mesh.RecalculateTangents();
+            mesh.RecalculateBounds();
+            mesh.OptimizeReorderVertexBuffer();
+            return mesh;
+        }
         
 
     }
---
+++ UMI3D-SDK/Assets/EnvironmentDevelopmentKit/Core/Runtime/Scene Design/Volumes/Cylinder.cs
@@ -0,0 +1,48 @@
+﻿/*
+Copyright 2019 - 2021 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using umi3d.common;
+using umi3d.common.volume;
+using UnityEngine;
+
+namespace umi3d.edk.volume
+{
+
+    public class Cylinder : AbstractPrimitive
+    {
+        public float radius = 1;
+        public float height = 3;
+
+        public override IEntity ToEntityDto(UMI3DUser user)
+        {
+            return new CylinderDto()
+            {
+                id = Id(),
+                center = this.transform.position,
+                rotation = this.transform.rotation,
+                scale = this.transform.localScale,
+                height = height,
+                radius = radius
+            };
+        }
+
+        public void OnDrawGizmos()
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireMesh(GeometryTools.GetCylinder(this.transform.position, this.transform.rotation, this.transform.localScale, radius, height));
+        }
+    }
+}
