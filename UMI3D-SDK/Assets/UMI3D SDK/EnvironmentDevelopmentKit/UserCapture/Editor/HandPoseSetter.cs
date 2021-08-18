906f3947dedce20251241c95589acae35a7e562a
Theo Desbonnets <theo.desbonnets@inetum.com>
2021-08-18 12:00:36 +0200

    Update version. Add if editor and header. disable forge about window automatic display

--- UMI3D-SDK/Assets/UMI3D SDK/EnvironmentDevelopmentKit/UserCapture/Editor/HandPoseSetter.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/EnvironmentDevelopmentKit/UserCapture/Editor/HandPoseSetter.cs
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+#if UNITY_EDITOR
 
 using System.Collections;
 using System.Collections.Generic;
@@ -222,3 +223,4 @@ namespace umi3d.edk.userCapture
         }
     }
 }
+#endif
