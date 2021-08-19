50e9230fe4997ed003a2a9f4e60cf6204b92735c
Nicolas Pacqueriaud <nicolas.pacqueriaud@gfi.fr>
2021-08-19 12:51:45 +0200

    Updating binding for fixing bytables

--- UMI3D-SDK/Assets/UMI3D SDK/Common/Collaboration/Runtime/UMI3DCollaborationNetworkingModule.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/Common/Collaboration/Runtime/UMI3DCollaborationNetworkingModule.cs
@@ -46,6 +46,7 @@ namespace umi3d.common.collaboration
                     bool active;
                     uint boneType;
                     ulong objectId;
+                    bool syncPosition;
                     SerializableVector3 offsetPosition;
                     SerializableVector4 offsetRotation;
                     if (UMI3DNetworkingHelper.TryRead(container, out bindingId)
@@ -54,7 +55,8 @@ namespace umi3d.common.collaboration
                         && UMI3DNetworkingHelper.TryRead(container, out boneType)
                         && UMI3DNetworkingHelper.TryRead(container, out objectId)
                         && UMI3DNetworkingHelper.TryRead(container, out offsetPosition)
-                        && UMI3DNetworkingHelper.TryRead(container, out offsetRotation))
+                        && UMI3DNetworkingHelper.TryRead(container, out offsetRotation)
+                        && UMI3DNetworkingHelper.TryRead(container, out syncPosition))
                     {
                         var bone = new BoneBindingDto()
                         {
@@ -64,7 +66,8 @@ namespace umi3d.common.collaboration
                             boneType = boneType,
                             objectId = objectId,
                             offsetPosition = offsetPosition,
-                            offsetRotation = offsetRotation
+                            offsetRotation = offsetRotation,
+                            syncPosition = syncPosition
                         };
                         result = (T)Convert.ChangeType(bone, typeof(T));
                         readable = true;
