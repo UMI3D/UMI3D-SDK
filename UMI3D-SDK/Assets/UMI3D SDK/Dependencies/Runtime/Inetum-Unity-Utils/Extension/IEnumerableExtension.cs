ca742177d098803428599d89643bf6ed6e414770
Theo Desbonnets <theo.desbonnets@inetum.com>
2021-10-25 12:53:48 +0200

    Update utils. Foreach back to void and Ienumerable version is now called Do

--- UMI3D-SDK/Assets/UMI3D SDK/Dependencies/Runtime/Inetum-Unity-Utils/Extension/IEnumerableExtension.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/Dependencies/Runtime/Inetum-Unity-Utils/Extension/IEnumerableExtension.cs
@@ -75,12 +75,14 @@ namespace inetum.unityUtils
 
 
         /// <summary>
-        /// Do an action for each element. Could be use full for debug purpose.
+        /// Do an action for each element when they are requested. 
+        /// For an instant call of the action see <see cref="ForEach{A}(IEnumerable{A}, Action{A})"/>
         /// </summary>
         /// <typeparam name="A"></typeparam>
         /// <param name="source"></param>
         /// <param name="action"></param>
-        public static IEnumerable<A> ForEach<A>(this IEnumerable<A> source, Action<A> action)
+        /// <returns></returns>
+        public static IEnumerable<A> Do<A>(this IEnumerable<A> source, Action<A> action)
         {
             if (action == null)
                 throw new Exception("action should not be null");
@@ -89,6 +91,24 @@ namespace inetum.unityUtils
                 {
                     action.Invoke(it.Current);
                     yield return it.Current;
+                }
+        }
+
+        /// <summary>
+        /// Do an action for each element. Could be use full for debug purpose.
+        /// For an IEnimerable call see <see cref="Do{A}(IEnumerable{A}, Action{A})"/>
+        /// </summary>
+        /// <typeparam name="A"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="action"></param>
+        public static void ForEach<A>(this IEnumerable<A> source, Action<A> action)
+        {
+            if (action == null)
+                throw new Exception("action should not be null");
+            using (var it = source.GetEnumerator())
+                while (it.MoveNext())
+                {
+                    action.Invoke(it.Current);
                 }
         }
 
