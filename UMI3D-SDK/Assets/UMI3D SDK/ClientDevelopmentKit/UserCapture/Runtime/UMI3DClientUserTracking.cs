b87ac729763e23f1274998aa296238577f1c89cc
Nicolas Pacqueriaud <nicolas.pacqueriaud@gfi.fr>
2022-09-19 16:54:22 +0200

    Generic rotation calcul for trackingframe

--- UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/UserCapture/Runtime/UMI3DClientUserTracking.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/UserCapture/Runtime/UMI3DClientUserTracking.cs
@@ -370,7 +370,7 @@ namespace umi3d.cdk.userCapture
                 }
 
                 Vector3 position = UMI3DNavigation.Instance.transform.localPosition;
-                Quaternion rotation = UMI3DNavigation.Instance.transform.localRotation;
+                Quaternion rotation = UMI3DNavigation.Instance.transform.localRotation * Quaternion.Inverse(UMI3DNavigation.Instance.transform.rotation) * transform.rotation;
 
                 if (!HasPlayerMoved(position, rotation, bonesList) && !forceNotNullDto && (Time.realtimeSinceStartup < lastTimeFrameSent + maximumTimeBetweenFramesSent))
                 {
