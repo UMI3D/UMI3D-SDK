606eff9ad38b196c0ac1c29c3d575467bbfb41ca
Theo Desbonnets <theo.desbonnets@inetum.com>
2022-09-27 09:02:35 +0200

    update version to 2.5.Debug.220927

--- UMI3D-SDK/Assets/UMI3D SDK/Common/Core/Runtime/UMI3DVersion.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/Common/Core/Runtime/UMI3DVersion.cs
@@ -21,7 +21,7 @@ namespace umi3d
         public static string version => major + "." + minor + "." + status + "." + date;
         public static readonly string major = "2";
         public static readonly string minor = "5";
-        public static readonly string status = "B";
-        public static readonly string date = "220923";
+        public static readonly string status = "Debug";
+        public static readonly string date = "220927";
     }
 }
