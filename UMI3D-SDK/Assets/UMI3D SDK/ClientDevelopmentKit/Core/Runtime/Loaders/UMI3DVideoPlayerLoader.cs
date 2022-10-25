022108b865cb1fb49b3b92a43af08ccf32ae7958
Theo Desbonnets <theo.desbonnets@inetum.com>
2022-10-25 15:45:50 +0200

    wait for video to be ready

--- UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/Core/Runtime/Loaders/UMI3DVideoPlayerLoader.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/Core/Runtime/Loaders/UMI3DVideoPlayerLoader.cs
@@ -17,6 +17,7 @@ limitations under the License.
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using umi3d.common;
 using UnityEngine;
@@ -33,7 +34,7 @@ namespace umi3d.cdk
         /// </summary>
         public static Queue<UMI3DVideoPlayerDto> videoPlayersToLoad = new Queue<UMI3DVideoPlayerDto>();
 
-        public static bool HasVideoToLoad => videoPlayersToLoad.Count > 0;
+        public static bool HasVideoToLoad => videoPlayersToLoad.Count > 0 || UMI3DEnvironmentLoader.Entities().Select(e => e?.Object as UMI3DVideoPlayer).Any(v => (!v?.isPrepared) ?? false);
 
         /// <summary>
         /// Asks to load a <see cref="UMI3DVideoPlayer"/> from a <see cref="UMI3DVideoPlayerDto"/>. 
@@ -59,8 +60,13 @@ namespace umi3d.cdk
         /// </summary>
         public static async Task LoadVideoPlayers()
         {
-            if (HasVideoToLoad)
+            if (videoPlayersToLoad.Count > 0)
                 await LoadVideoPlayersCoroutine();
+            while (HasVideoToLoad)
+            {
+                await UMI3DAsyncManager.Yield();
+            }
+            await UMI3DAsyncManager.Delay(100);
         }
 
         /// <summary>
@@ -72,20 +78,17 @@ namespace umi3d.cdk
         {
             var wait = new WaitForSeconds(.1f);
 
-            await UMI3DAsyncManager.Delay(100);
-
             while (videoPlayersToLoad.Count > 0)
             {
+                await UMI3DAsyncManager.Delay(100);
+
                 UMI3DVideoPlayerDto videoPlayer = videoPlayersToLoad.Dequeue();
 
                 var player = new UMI3DVideoPlayer(videoPlayer);
 
                 while (!player.isPrepared)
                     await UMI3DAsyncManager.Yield();
-
-                await UMI3DAsyncManager.Delay(100);
             }
-
         }
     }
 }
