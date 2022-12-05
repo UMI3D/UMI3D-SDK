58ed6f843318013b45e95869fbe0e198704fd527
TheoDesbonnets <56262448+TheoDesbonnets@users.noreply.github.com>
2022-12-05 12:07:01 +0100

    Add Input Menu Item for new parameters

---
+++ UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/InteractionSystem/Runtime/Menu/Model/Input/ColorInputMenuItem.cs
@@ -0,0 +1,87 @@
+﻿/*
+Copyright 2019 - 2021 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace umi3d.cdk.menu
+{
+    /// <summary>
+    /// <see cref="MenuItem"/> for text input.
+    /// </summary>
+    public class ColorInputMenuItem : AbstractInputMenuItem<Color>
+    {
+        /// <summary>
+        /// Input value.
+        /// </summary>
+        private Color value = Color.white;
+
+        /// <summary>
+        /// Subscribers on value change
+        /// </summary>
+        private readonly List<Action<Color>> subscribers = new List<Action<Color>>();
+
+        /// <inheritdoc/>
+        public override Color GetValue()
+        {
+            return value;
+        }
+
+        /// <summary>
+        /// Notify a change of the input value.
+        /// </summary>
+        /// <param name="newValue"></param>
+        public override void NotifyValueChange(Color newValue)
+        {
+            value = newValue;
+            foreach (Action<Color> sub in subscribers)
+            {
+                sub.Invoke(newValue);
+            }
+        }
+
+
+
+        /// <summary>
+        /// Subscribe a callback for input value change.
+        /// </summary>
+        /// <param name="callback">Callback to invoke on input value change</param>
+        public override bool Subscribe(Action<Color> callback)
+        {
+            if (!subscribers.Contains(callback))
+            {
+                subscribers.Add(callback);
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// Unsubscribe a callback from the value change.
+        /// </summary>
+        /// <param name="callback"></param>
+        public override bool UnSubscribe(Action<Color> callback)
+        {
+           return subscribers.Remove(callback);
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/InteractionSystem/Runtime/Menu/Model/Input/Vector4InputMenuItem.cs
@@ -0,0 +1,87 @@
+﻿/*
+Copyright 2019 - 2021 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace umi3d.cdk.menu
+{
+    /// <summary>
+    /// <see cref="MenuItem"/> for text input.
+    /// </summary>
+    public class Vector4InputMenuItem : AbstractInputMenuItem<Vector4>
+    {
+        /// <summary>
+        /// Input value.
+        /// </summary>
+        private Vector4 value = Vector4.zero;
+
+        /// <summary>
+        /// Subscribers on value change
+        /// </summary>
+        private readonly List<Action<Vector4>> subscribers = new List<Action<Vector4>>();
+
+        /// <inheritdoc/>
+        public override Vector4 GetValue()
+        {
+            return value;
+        }
+
+        /// <summary>
+        /// Notify a change of the input value.
+        /// </summary>
+        /// <param name="newValue"></param>
+        public override void NotifyValueChange(Vector4 newValue)
+        {
+            value = newValue;
+            foreach (Action<Vector4> sub in subscribers)
+            {
+                sub.Invoke(newValue);
+            }
+        }
+
+
+
+        /// <summary>
+        /// Subscribe a callback for input value change.
+        /// </summary>
+        /// <param name="callback">Callback to invoke on input value change</param>
+        public override bool Subscribe(Action<Vector4> callback)
+        {
+            if (!subscribers.Contains(callback))
+            {
+                subscribers.Add(callback);
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// Unsubscribe a callback from the value change.
+        /// </summary>
+        /// <param name="callback"></param>
+        public override bool UnSubscribe(Action<Vector4> callback)
+        {
+           return subscribers.Remove(callback);
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
