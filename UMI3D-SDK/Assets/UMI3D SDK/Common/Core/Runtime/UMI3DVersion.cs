4b1986411bd890b3444d2216ffb051ebc6b399d1
TheoDesbonnets <56262448+TheoDesbonnets@users.noreply.github.com>
2023-01-10 18:48:40 +0100

    remove using added by mistake

--- UMI3D-SDK/Assets/UMI3D SDK/Common/Core/Runtime/UMI3DVersion.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/Common/Core/Runtime/UMI3DVersion.cs
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-using ICSharpCode.NRefactory.Ast;
 using System;
 using System.Globalization;
 
