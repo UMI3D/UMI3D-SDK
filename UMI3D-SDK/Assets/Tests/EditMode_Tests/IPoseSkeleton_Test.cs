48dc587e072cc2286edfd3f8b26c408adf861d56
Vladimir Chantitch <96313033+VladimirChantitch@users.noreply.github.com>
2023-01-27 12:14:04 +0100

    Mock test implementation WIP

--- UMI3D-SDK/Assets/Tests/EditMode_Tests/IPoseSkeleton_Test.cs
+++ UMI3D-SDK/Assets/Tests/EditMode_Tests/IPoseSkeleton_Test.cs
@@ -70,17 +70,20 @@ public class IPoseSkeleton_Test
     [Test]
     public void Test_Compute_OneAnimatedSkeletonWithhBones()
     {
-        //Given
-        ISkeleton iskeletton = (collaborativeSkeleton as ISkeleton);
-        animatedSkeletons.Add(new AnimatedSkeleton());
-        animatedSkeletons.Add(new AnimatedSkeleton());
+        //Mock<ExternalStuff> mock = new Mock<ExternalStuff>();
+        ////Lets mock the method of interest
+        //mock.Setup(x => x.NameLengthInMyImaginaryWorld("Jean-Paul Muller")).Returns(159);
+        ////Given
+        //ISkeleton iskeletton = (collaborativeSkeleton as ISkeleton);
+        //animatedSkeletons.Add(new AnimatedSkeleton());
+        //animatedSkeletons.Add(new AnimatedSkeleton());
 
-        iskeletton.Skeletons = animatedSkeletons.ToArray();
+        //iskeletton.Skeletons = animatedSkeletons.ToArray();
 
-        //When
-        ISkeleton results = iskeletton.Compute();
+        ////When
+        //ISkeleton results = iskeletton.Compute();
 
-        //Then
-        Assert.IsTrue(results.Bones.Count == 0);
+        ////Then
+        //Assert.IsTrue(results.Bones.Count == 0);
     }
 }
