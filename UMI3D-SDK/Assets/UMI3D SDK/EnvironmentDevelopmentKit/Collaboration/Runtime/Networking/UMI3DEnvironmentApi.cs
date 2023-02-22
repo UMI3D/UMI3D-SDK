7e3222aba8c1d9159bbc23bff1221db447c9c643
Quentitin-Tran <quentin.tran@inetum.com>
2023-02-22 15:29:18 +0100

    Fix to connect environment

--- UMI3D-SDK/Assets/UMI3D SDK/EnvironmentDevelopmentKit/Collaboration/Runtime/Networking/UMI3DEnvironmentApi.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/EnvironmentDevelopmentKit/Collaboration/Runtime/Networking/UMI3DEnvironmentApi.cs
@@ -64,10 +64,17 @@ namespace umi3d.edk.collaboration
         [HttpGet(UMI3DNetworkingKeys.connectionInfo, WebServiceMethodAttribute.Security.Private, WebServiceMethodAttribute.Type.Method)]
         public void GetConnectionInformation(object sender, HttpRequestEventArgs e, Dictionary<string, string> uriparam)
         {
-            UMI3DCollaborationUser user = GetUserFor(e.Request);
-            UMI3DLogger.Log($"Get Connection Information {user?.Id()}", scope);
-            var connectionInformation = user.ToUserConnectionDto();
-            e.Response.WriteContent(connectionInformation.ToBson());
+            try
+            {
+                UMI3DCollaborationUser user = GetUserFor(e.Request);
+                UMI3DLogger.Log($"Get Connection Information {user?.Id()}", scope);
+                var connectionInformation = user.ToUserConnectionDto();
+                e.Response.WriteContent(connectionInformation.ToBson());
+            } catch (Exception ex)
+            {
+                UMI3DLogger.LogError($"Exception Get Connection Information", scope);
+                UMI3DLogger.LogException(ex, scope);
+            }
         }
 
         /// <summary>
