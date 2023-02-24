61c5dfef52f1025d384e87a2941cc423bf217a20
CMarichal <charles-eole.marichal@inetum.com>
2023-02-24 11:42:55 +0100

    Cleaned SkeletonNodeDto imports

--- UMI3D-SDK/Assets/UMI3D SDK/Common/UserCapture/Runtime/New/UMI3DSkeletonNodeDto.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/Common/UserCapture/Runtime/New/UMI3DSkeletonNodeDto.cs
@@ -14,13 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-
-using System;
-using System.Collections.Generic;
-using umi3d.common;
-using UnityEditor;
-using UnityEngine;
-
 namespace umi3d.common.userCapture
 {
     /// <summary>
