ae8c059cbc1238cadcacafd2ce4c8aee48efe154
CMarichal <charles-eole.marichal@inetum.com>
2023-04-27 11:56:37 +0200

    Create EDK bindings
    Deleted old EDK binding

---
+++ UMI3D-SDK/Assets/UMI3D SDK/EnvironmentDevelopmentKit/Core/Runtime/Scene synchronisation/Binding/Operations/AbstractSingleBinding.cs
@@ -0,0 +1,63 @@
+﻿/*
+Copyright 2019 - 2023 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using UnityEngine;
+
+namespace umi3d.edk
+{
+    /// <summary>
+    /// Operation binding a node and only one object.
+    /// </summary>
+    public abstract class AbstractSingleBinding : AbstractBinding
+    {
+        /// <summary>
+        /// Should the parent object translation movements be applied to the bound point?
+        /// </summary>
+        public bool syncPosition = false;
+
+        /// <summary>
+        /// Should the parent object rotation movements be applied to the bound point? <br/>
+        /// When combined with position synchronisation, resulting movement is relative to an anchor.
+        /// </summary>
+        public bool syncRotation = false;
+
+        /// <summary>
+        /// Should the parent object scaling be applied to the bound point?
+        /// </summary>
+        public bool syncScale = false;
+
+        /// <summary>
+        /// Offset between the parent position and the bound object target position.
+        /// </summary>
+        public Vector3 offsetPosition = Vector3.zero;
+
+        /// <summary>
+        /// Offset between the parent rotation and the bound object target rotation.
+        /// </summary>
+        public Quaternion offsetRotation = Quaternion.identity;
+
+        /// <summary>
+        /// Offset between the parent scale and the bound object target scale.
+        /// </summary>
+        public Vector3 offsetScale = Vector3.zero;
+
+        /// <summary>
+        /// Anchor for rotation when position and rotation synchronisation are active. <br/>
+        /// Value is given in the parent object referential.
+        /// </summary>
+        public Vector3 anchor = Vector3.zero;
+    }
+}
---
+++ UMI3D-SDK/Assets/UMI3D SDK/EnvironmentDevelopmentKit/UserCapture/Runtime/Tracking/Binding/RiggedBoneBinding.cs
@@ -0,0 +1,68 @@
+﻿/*
+Copyright 2019 - 2021 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using umi3d.common;
+using umi3d.common.userCapture;
+using UnityEngine;
+
+namespace umi3d.edk.userCapture
+{
+    /// <summary>
+    /// Operation binding a rig under a node to a user's skeleton bone.
+    /// </summary>
+    public class RiggedBoneBinding : BoneBinding
+    {
+        /// <summary>
+        /// Name of the rig to bind under the declared bound node.
+        /// </summary>
+        public string rigName = "";
+
+        public RiggedBoneBinding() { }
+
+        /// <inheritdoc/>
+        public override BindingDto ToDto()
+        {
+            AbstractBindingDataDto bindingDataDto;
+
+            bindingDataDto = new RiggedBoneBindingDataDto(
+                userId: userId,
+                boneType: boneType,
+                rigName: rigName,
+
+                offSetPosition: offsetPosition,
+                offSetRotation: offsetRotation,
+                offSetScale: offsetScale,
+
+                anchorPosition: anchor,
+
+                partialFit: partialFit,
+                priority: priority,
+
+                syncPosition: syncPosition,
+                syncRotation: syncRotation,
+                syncScale: syncScale
+            );
+
+
+            BindingDto bindingDto = new BindingDto(
+                boundNodeId: boundNodeId,
+                data: bindingDataDto
+            );
+
+            return bindingDto;
+        }
+    }
+}
