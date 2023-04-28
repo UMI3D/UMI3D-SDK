89b6a145e70f02114ec14c99c9c0af7493c3ac15
TheoDesbonnets <56262448+TheoDesbonnets@users.noreply.github.com>
2023-04-28 11:21:25 +0200

    remove start and update in CollaborativeSkeletonsScene

--- UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/Collaboration/Runtime/TrackingKalmanFilters/CollaborativeSkeletonsScene.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/Collaboration/Runtime/TrackingKalmanFilters/CollaborativeSkeletonsScene.cs
@@ -20,16 +20,4 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CollaborativeSkeletonsScene : SingleBehaviour<CollaborativeSkeletonsScene>
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+{ }
