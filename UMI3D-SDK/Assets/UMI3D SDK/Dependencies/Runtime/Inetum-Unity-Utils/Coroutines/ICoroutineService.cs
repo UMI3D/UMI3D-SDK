a2b6659a490da24edce8868ec13bb70ab8633f99
CMarichal <charles-eole.marichal@inetum.com>
2023-05-17 12:16:36 +0200

    Add coroutine manager
    - Extract coroutine logic from UMI3DEnvironmentLoadingHandler in a SingleBehaviour
    - Support for late coroutines, that are called during LateUpdate

---
+++ UMI3D-SDK/Assets/UMI3D SDK/Dependencies/Runtime/Inetum-Unity-Utils/Coroutines/ICoroutineService.cs
@@ -0,0 +1,54 @@
+﻿/*
+Copyright 2019 - 2023 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System.Collections;
+
+using UnityEngine;
+
+namespace inetum.unityUtils
+{
+    /// <summary>
+    /// Service that provide access to a monobehaviour running unity coroutines
+    /// </summary>
+    public interface ICoroutineService
+    {
+        // <summary>
+        /// Call this method to attach a coroutine to the handler and start it.
+        /// </summary>
+        public Coroutine AttachCoroutine(IEnumerator coroutine);
+
+        /// <summary>
+        /// Call this method to remove a coroutine from the handler and stop it.
+        /// </summary>
+        public void DettachCoroutine(Coroutine coroutine);
+    }
+
+    /// <summary>
+    /// Routine service for coroutine to call during late update.
+    /// </summary>
+    public interface ILateRoutineService
+    {
+        // <summary>
+        /// Call this method to attach a late update routine to the handler and start it.
+        /// </summary>
+        public IEnumerator AttachLateRoutine(IEnumerator routine);
+
+        /// <summary>
+        /// Call this method to remove a late update routine from the handler and stop it.
+        /// </summary>
+        public void DettachLateRoutine(IEnumerator routine);
+    }
+}
