e3a2f0180b23907071a40e67ec72c3d96f0d11d4
Vladimir Chantitch <96313033+VladimirChantitch@users.noreply.github.com>
2023-06-27 16:26:16 +0200

    Fix Personnal skeleton

--- UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/UserCapture/Runtime/Skeleton/PersonalSkeleton.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/UserCapture/Runtime/Skeleton/PersonalSkeleton.cs
@@ -26,7 +26,7 @@ namespace umi3d.cdk.userCapture
 
         public Vector3 worldSize => TrackedSkeleton.transform.lossyScale;
 
-        protected override void Start()
+        protected void Start()
         {
             PoseSkeleton = new PoseSkeleton();
             Skeletons = new List<ISubSkeleton>
