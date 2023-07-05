956f6e7c7050a28e773d43179dfc125f5944d988
Nicolas Pacqueriaud <nicolas.pacqueriaud@inetum.com>
2023-07-05 11:27:20 +0200

    Fix Bones Computing
    Preventing the bones set by the TrackedSkeleton from the application of the Hips rotation.

--- UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/UserCapture/Runtime/Skeleton/AbstractSkeleton.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/UserCapture/Runtime/Skeleton/AbstractSkeleton.cs
@@ -88,6 +88,12 @@ namespace umi3d.cdk.userCapture
         private Dictionary<uint, bool> alreadyComputedBonesCache = new();
 
         /// <summary>
+        /// Containing id of the bones set by the TrackedSkeleton in <see cref="RetrieveBonesRotation(UMI3DSkeletonHierarchy)"/> method.
+        /// Preventing from the application of the Hips rotation to these bones in <see cref="ComputeBonePosition(uint)"/> method.
+        /// </summary>
+        private List<uint> bonesSetByTrackedSkeleton = new();
+
+        /// <summary>
         /// Compute the final position of each bone, and their parents recursively if not already computed
         /// </summary>
         /// <param name="boneType"></param>
@@ -101,7 +107,10 @@ namespace umi3d.cdk.userCapture
                     ComputeBonePosition(boneRelation.boneTypeParent);
 
                 Bones[boneType].s_Position = Bones[boneRelation.boneTypeParent].s_Position + Bones[boneRelation.boneTypeParent].s_Rotation * boneRelation.relativePosition;
-                Bones[boneType].s_Rotation = Bones[BoneType.Hips].s_Rotation * Bones[boneType].s_Rotation; // all global bones rotations should be turned the same way as the anchor
+
+                if (!bonesSetByTrackedSkeleton.Contains(boneType))
+                    Bones[boneType].s_Rotation = Bones[BoneType.Hips].s_Rotation * Bones[boneType].s_Rotation; // all global bones rotations should be turned the same way as the anchor
+                
                 alreadyComputedBonesCache[boneType] = true;
             }
         }
@@ -120,6 +129,8 @@ namespace umi3d.cdk.userCapture
                 else
                     Bones[bone] = new ISkeleton.s_Transform() { s_Rotation = Quaternion.identity };
             }
+
+            bonesSetByTrackedSkeleton.Clear();
 
             // for each subskeleton, in descending order (lastest has lowest priority),
             // get the relative orientation of all available bones
@@ -150,6 +161,14 @@ namespace umi3d.cdk.userCapture
                         Bones[b.boneType].s_Rotation = b.rotation.Quaternion();
                     else
                         Bones.Add(b.boneType, new ISkeleton.s_Transform() { s_Rotation = b.rotation.Quaternion() });
+                }
+
+                if (i == 0) //the TrackedSkeleton is the first SubSkeleton
+                {
+                    foreach (var b in bones)
+                    {
+                        bonesSetByTrackedSkeleton.Add(b.boneType);
+                    }
                 }
             }
         }
