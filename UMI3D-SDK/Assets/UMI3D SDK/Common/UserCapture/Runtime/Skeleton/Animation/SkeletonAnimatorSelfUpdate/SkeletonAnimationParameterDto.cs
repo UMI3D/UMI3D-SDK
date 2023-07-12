87ac7341e2595f1bfec955f77516e470920d8433
TheoDesbonnets <56262448+TheoDesbonnets@users.noreply.github.com>
2023-07-12 14:11:07 +0200

    Add getter setter in skeletonAnimator dto

--- UMI3D-SDK/Assets/UMI3D SDK/Common/UserCapture/Runtime/Skeleton/Animation/SkeletonAnimatorSelfUpdate/SkeletonAnimationParameterDto.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/Common/UserCapture/Runtime/Skeleton/Animation/SkeletonAnimatorSelfUpdate/SkeletonAnimationParameterDto.cs
@@ -25,13 +25,13 @@ namespace umi3d.common.userCapture.animation
         /// Key of the parameter to compute in <see cref="SkeletonAnimatorParameterKeys"/>. <br/>
         /// Each key result in a different computation in browsers.
         /// </summary>
-        public uint parameterKey;
+        public uint parameterKey { get; set; }
 
         /// <summary>
         /// Ranges to clamp value to a constant result when in an interval. <br/>
         /// "If no range is defined, the parameter value is directly given to the animator.
         /// </summary>
-        public RangeDto[] ranges;
+        public RangeDto[] ranges { get; set; }
 
         /// <summary>
         ///  Ranges to clamp value to a certain result when in an interval.
@@ -41,22 +41,22 @@ namespace umi3d.common.userCapture.animation
             /// <summary>
             /// Start value of range
             /// </summary>
-            public float startBound;
+            public float startBound { get; set; }
 
             /// <summary>
             /// End value of range
             /// </summary>
-            public float endBound;
+            public float endBound { get; set; }
 
             /// <summary>
             /// Resulting value if the computed value is within <see cref="startBound"/> and <see cref="endBound"/>.
             /// </summary>
-            public float result;
+            public float result { get; set; }
 
             /// <summary>
             /// If true, raw value is used as result if computed value is within <see cref="startBound"/> and <see cref="endBound"/>.
             /// </summary>
-            public bool rawValue;
+            public bool rawValue { get; set; }
         }
     }
 }
