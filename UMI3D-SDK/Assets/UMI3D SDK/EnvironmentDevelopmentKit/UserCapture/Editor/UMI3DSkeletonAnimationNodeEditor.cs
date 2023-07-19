5d3a5cbfd2daee16ec21b9960ae8d2ac2f562648
TheoDesbonnets <56262448+TheoDesbonnets@users.noreply.github.com>
2023-07-19 10:35:50 +0200

    add if unity editor

--- UMI3D-SDK/Assets/UMI3D SDK/EnvironmentDevelopmentKit/UserCapture/Editor/UMI3DSkeletonAnimationNodeEditor.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/EnvironmentDevelopmentKit/UserCapture/Editor/UMI3DSkeletonAnimationNodeEditor.cs
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
+#if UNITY_EDITOR
 using umi3d.common.userCapture.animation;
 using umi3d.edk.editor;
 using UnityEditor;
@@ -79,3 +79,4 @@ namespace umi3d.edk.userCapture.animation.editor
         }
     }
 }
+#endif
