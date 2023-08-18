a8cc4abbf6e7bc1a637ab96e8975729db5bf925b
Romain Tirbisch <romain.tirbisch@inetum.com>
2023-08-18 14:40:07 +0200

    Rename LauncherOnMasterServer

--- UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/Collaboration/Runtime/UMI3DNetworking scripts/UMI3DNetworking.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/Collaboration/Runtime/UMI3DNetworking scripts/UMI3DNetworking.cs
@@ -14,11 +14,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-using System.Collections.Generic;
+using BeardedManStudios.Forge.Networking;
+using inetum.unityUtils;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using umi3d.debug;
 using UnityEngine.Networking;
-using System.Collections;
 
 namespace umi3d.cdk
 {
@@ -50,6 +52,115 @@ namespace umi3d.cdk
                 shouldCleanAbort, onCompleteSuccess, onCompleteFail,
                 report
             );
+        }
+
+        #endregion
+
+        #region LauncherOnMasterServer
+
+        /// <summary>
+        /// Event raise when the connection to a master server failed.
+        /// </summary>
+        public static event Action connectFailed_LoMS
+        {
+            add
+            {
+                LauncherOnMasterServer.connectFailed += value;
+            }
+            remove
+            {
+                LauncherOnMasterServer.connectFailed += value;
+            }
+        }
+
+        /// <summary>
+        /// Event raise when the connection to a master server succeeded.
+        /// </summary>
+        public static event Action connectSucceeded_LoMS
+        {
+            add
+            {
+                LauncherOnMasterServer.connectSucceeded += value;
+            }
+            remove
+            {
+                LauncherOnMasterServer.connectSucceeded += value;
+            }
+        }
+
+        /// <summary>
+        /// Event raise when a request info to a master server has succeeded.
+        /// </summary>
+        public static event Action<(string serverName, string icon)> requestServerInfSucceeded_LoMS
+        {
+            add
+            {
+                LauncherOnMasterServer.requestServerInfSucceeded += value;
+            }
+            remove
+            {
+                LauncherOnMasterServer.requestServerInfSucceeded += value;
+            }
+        }
+
+        /// <summary>
+        /// Event raise when a request info has succeeded.
+        /// </summary>
+        public static event Action<MasterServerResponse.Server> requestSessionInfSucceeded
+        {
+            add
+            {
+                LauncherOnMasterServer.requestSessionInfSucceeded += value;
+            }
+            remove
+            {
+                LauncherOnMasterServer.requestSessionInfSucceeded -= value;
+            }
+        }
+
+        /// <summary>
+        /// Try to connect to a master server asyncronously.
+        /// 
+        /// <para>
+        /// The connection is established in another thread. The <see cref="connectFailed"/> and <see cref="connectSucceeded"/> events are raised in the main-thread.
+        /// </para>
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static AsyncOperation ConnectAsync_LoMS(string url)
+        {
+            return LauncherOnMasterServer.ConnectAsync(url);
+        }
+
+        /// <summary>
+        /// Disconnect a master server.
+        /// </summary>
+        public static void Disconnect_LoMS()
+        {
+            LauncherOnMasterServer.Disconnect();
+        }
+
+        /// <summary>
+        /// Get the requested information about this master server asyncronously.
+        /// 
+        /// <para>
+        /// The request is performed in another thread.
+        /// </para>
+        /// </summary>
+        /// <param name="requestFailed"></param>
+        public static void RequestServerInfo_LoMS(Action requestFailed)
+        {
+            LauncherOnMasterServer.RequestServerInfo(requestFailed);
+        }
+
+        /// <summary>
+        /// Get the requested information about this master server's <paramref name="sessionId"/> asyncronously.
+        /// </summary>
+        /// <param name="sessionId"></param>
+        /// <param name="requestFailed"></param>
+        public static void RequestSessionInfo_LoMS(string sessionId, Action requestFailed)
+        {
+            LauncherOnMasterServer.RequestSessionInfo(sessionId, requestFailed);
         }
 
         #endregion
