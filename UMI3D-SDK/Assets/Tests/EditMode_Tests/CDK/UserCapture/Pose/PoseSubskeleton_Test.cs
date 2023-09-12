ad14c4c1232322f017140d20c091c6d22f589c3f
CMarichal <charles-eole.marichal@inetum.com>
2023-09-12 16:59:59 +0200

    Add unit tests on PoseSubskeleton

--- UMI3D-SDK/Assets/Tests/EditMode_Tests/CDK/UserCapture/Pose/PoseSubskeleton_Test.cs
+++ UMI3D-SDK/Assets/Tests/EditMode_Tests/CDK/UserCapture/Pose/PoseSubskeleton_Test.cs
@@ -16,19 +16,26 @@ limitations under the License.
 
 using Moq;
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using umi3d.cdk.userCapture.pose;
+using umi3d.common.userCapture;
+using umi3d.common.userCapture.description;
 using umi3d.common.userCapture.pose;
+using umi3d.common.userCapture.tracking;
+using UnityEngine;
 
 namespace EditMode_Tests.UserCapture.Pose.CDK
 {
-    using SkeletonPose = umi3d.cdk.userCapture.pose.SkeletonPose;
-
+    [TestFixture, TestOf(nameof(PoseSubskeleton))]
     public class PoseSubskeleton_Test
     {
         private PoseSubskeleton poseSubskeleton = null;
 
         private Mock<IPoseManager> poseManagerServiceMock;
+
+        #region SetUp
 
         [OneTimeSetUp]
         public void OneTimeSetUp()
@@ -56,16 +63,53 @@ namespace EditMode_Tests.UserCapture.Pose.CDK
                 PoseManager.Destroy();
         }
 
-        #region SetPose
+        #endregion SetUp
+
+        #region StartPose
 
         [Test]
-        public void SetPose_Null()
+        public void StartPose_Null()
         {
-            poseSubskeleton.StartPose(new List<SkeletonPose>(), true);
+            // given
+            List<SkeletonPose> poses = null;
+
+            // when
+            TestDelegate action = () => poseSubskeleton.StartPose(poses, true);
+
+            // then
+            Assert.Throws<ArgumentNullException>(() => action());
         }
 
         [Test]
-        public void SetPoses()
+        public void StartPose_Empty()
+        {
+            // given
+            List<SkeletonPose> poses = new();
+
+            // when
+            TestDelegate action = () => poseSubskeleton.StartPose(poses, true);
+
+            // then
+            Assert.DoesNotThrow(() => action());
+            Assert.AreEqual(poses.Count, poseSubskeleton.AppliedPoses.Count);
+        }
+
+        [Test]
+        public void StartPose_One()
+        {
+            // GIVEN
+            var pose = new SkeletonPose(new PoseDto(), true);
+
+            // WHEN
+            poseSubskeleton.StartPose(pose, true);
+
+            // THEN
+            Assert.AreEqual(1, poseSubskeleton.AppliedPoses.Count);
+            Assert.AreEqual(pose, poseSubskeleton.AppliedPoses[0]);
+        }
+
+        [Test]
+        public void StartPose_Many()
         {
             // GIVEN
             var poses = new List<SkeletonPose>()
@@ -76,12 +120,79 @@ namespace EditMode_Tests.UserCapture.Pose.CDK
 
             // WHEN
             poseSubskeleton.StartPose(poses, true);
-            
+
             // THEN
-            Assert.IsTrue(poseSubskeleton.AppliedPoses.Count == 2);
+            Assert.AreEqual(poses.Count, poseSubskeleton.AppliedPoses.Count);
         }
 
-        #endregion SetPose
+        #endregion StartPose
+
+        #region StopPose
+
+        [Test]
+        public void StopPose_Null()
+        {
+            // GIVEN
+            SkeletonPose pose = null;
+
+            // WHEN
+            poseSubskeleton.StopPose(pose);
+
+            // THEN
+            Assert.AreEqual(0, poseSubskeleton.AppliedPoses.Count);
+        }
+
+        [Test]
+        public void StopPose_Empty()
+        {
+            // GIVEN
+            List<SkeletonPose> pose = new();
+
+            // WHEN
+            poseSubskeleton.StopPose(pose);
+
+            // THEN
+            Assert.AreEqual(0, poseSubskeleton.AppliedPoses.Count);
+        }
+
+        [Test]
+        public void StopPose_One()
+        {
+            // GIVEN
+            var pose = new SkeletonPose(new PoseDto(), true);
+
+            poseSubskeleton.StartPose(pose, true);
+
+            // WHEN
+            poseSubskeleton.StopPose(pose);
+
+            // THEN
+            Assert.AreEqual(0, poseSubskeleton.AppliedPoses.Count);
+        }
+
+        [Test]
+        public void StopPose_Many()
+        {
+            // GIVEN
+            var poses = new List<SkeletonPose>()
+            {
+                new SkeletonPose(new PoseDto(), true),
+                new SkeletonPose(new PoseDto()),
+                new SkeletonPose(new PoseDto()),
+            };
+
+            var posesToStop = poses.ToArray()[0..2];
+
+            poseSubskeleton.StartPose(poses, true);
+
+            // WHEN
+            poseSubskeleton.StopPose(posesToStop);
+
+            // THEN
+            Assert.AreEqual(1, poseSubskeleton.AppliedPoses.Count);
+        }
+
+        #endregion StopPose
 
         #region StopAllPoses
 
@@ -101,9 +212,196 @@ namespace EditMode_Tests.UserCapture.Pose.CDK
             poseSubskeleton.StopAllPoses();
 
             // THEN
-            Assert.IsTrue(poseSubskeleton.AppliedPoses.Count == 0);
+            Assert.AreEqual(0, poseSubskeleton.AppliedPoses.Count);
         }
 
         #endregion StopAllPoses
+
+        #region GetPose
+
+        [Test]
+        public void GetPose_NoPoseSet()
+        {
+            // Given
+            HierarchyDefinitionLocal hierarchyDef = new();
+            hierarchyDef.Relations.Add(new(BoneType.Chest, BoneType.None, Vector3.zero));
+            hierarchyDef.Relations.Add(new(BoneType.Spine, BoneType.Chest, Vector3.zero));
+            hierarchyDef.Relations.Add(new(BoneType.LeftForearm, BoneType.Chest, Vector3.zero));
+
+            UMI3DSkeletonHierarchy hierarchy = new UMI3DSkeletonHierarchy(hierarchyDef);
+
+            // When
+            var result = poseSubskeleton.GetPose(hierarchy); //hierarchy parameter is not important
+
+            // Then
+            Assert.AreEqual(0, result.bones.Count);
+        }
+
+        private class HierarchyDefinitionLocal : IUMI3DSkeletonHierarchyDefinition
+        {
+            public IList<UMI3DSkeletonHierarchyDefinition.BoneRelation> Relations => relations;
+
+            private readonly List<UMI3DSkeletonHierarchyDefinition.BoneRelation> relations = new();
+        }
+
+        [Test]
+        public void GetPose()
+        {
+            // Given
+            var poses = new List<SkeletonPose>()
+            {
+                new SkeletonPose(new PoseDto()
+                {
+                    bones = new()
+                    {
+                        new() { boneType = BoneType.Chest, rotation = new() },
+                        new() { boneType = BoneType.Spine, rotation = new() },
+                    }
+                }, true),
+            };
+
+            poseSubskeleton.StartPose(poses, false);
+
+            HierarchyDefinitionLocal hierarchyDef = new();
+            hierarchyDef.Relations.Add(new(BoneType.Chest, BoneType.None, Vector3.zero));
+            hierarchyDef.Relations.Add(new(BoneType.Spine, BoneType.Chest, Vector3.zero));
+            hierarchyDef.Relations.Add(new(BoneType.LeftForearm, BoneType.Chest, Vector3.zero));
+
+            UMI3DSkeletonHierarchy hierarchy = new UMI3DSkeletonHierarchy(hierarchyDef);
+
+            // When
+            var result = poseSubskeleton.GetPose(hierarchy);
+
+            // Then
+            Assert.AreEqual(2, result.bones.Count);
+        }
+
+        [Test]
+        public void GetPose_InvalidHierarchy()
+        {
+            // Given
+            var poses = new List<SkeletonPose>()
+            {
+                new SkeletonPose(new PoseDto()
+                {
+                    bones = new()
+                    {
+                        new() { boneType = BoneType.Chest, rotation = new() },
+                        new() { boneType = BoneType.Spine, rotation = new() },
+                        new() { boneType = 120u, rotation = new() },
+                    }
+                }, true),
+            };
+
+            poseSubskeleton.StartPose(poses, false);
+
+            HierarchyDefinitionLocal hierarchyDef = new();
+            hierarchyDef.Relations.Add(new(BoneType.Chest, BoneType.None, Vector3.zero));
+            hierarchyDef.Relations.Add(new(BoneType.Spine, BoneType.LeftToeBase, Vector3.zero));
+
+            UMI3DSkeletonHierarchy hierarchy = new UMI3DSkeletonHierarchy(hierarchyDef);
+
+            // When
+            TestDelegate action = () => poseSubskeleton.GetPose(hierarchy);
+
+            // Then
+            Assert.Throws<ArgumentException>(() => action());
+        }
+
+        [Test]
+        public void GetPose_Null()
+        {
+            // Given
+            UMI3DSkeletonHierarchy hierarchy = null;
+
+            // When
+            TestDelegate action = () => poseSubskeleton.GetPose(hierarchy);
+
+            // Then
+            Assert.Throws<ArgumentNullException>(() => action());
+        }
+
+        #endregion GetPose
+
+        #region UpdateBones
+
+        [Test]
+        public void UpdateBones_Null()
+        {
+            // Given
+            UserTrackingFrameDto frame = null;
+
+            // When
+            TestDelegate action = () => poseSubskeleton.UpdateBones(frame);
+
+            // Then
+            Assert.Throws<ArgumentNullException>(() => action());
+        }
+
+        [Test]
+        public void UpdateBones()
+        {
+            // Given
+            var poses = new List<SkeletonPose>()
+            {
+                new SkeletonPose(new PoseDto() { index=0, bones=new() { new() { boneType=BoneType.Chest } }, boneAnchor=new()}, true),
+                new SkeletonPose(new PoseDto() { index=1, bones=new() { new() { boneType=BoneType.Chest } }, boneAnchor=new()}, true),
+            };
+
+            UserTrackingFrameDto frame = new()
+            {
+                customPosesIndexes = new() { 1, 2 },
+                userId = 1005uL
+            };
+
+            poseManagerServiceMock.Setup(x => x.Poses).Returns(new Dictionary<ulong, IList<SkeletonPose>>() { { frame.userId, poses } });
+
+            // When
+            poseSubskeleton.UpdateBones(frame);
+
+            // Then
+            Assert.AreEqual(1, poseSubskeleton.AppliedPoses.Count);
+        }
+
+        #endregion UpdateBones
+
+        #region WriteTrackingFrame
+
+        [Test]
+        public void WriteTrackingFrame_Null()
+        {
+            // Given
+            UserTrackingFrameDto frame = null;
+
+            // When
+            TestDelegate action = () => poseSubskeleton.WriteTrackingFrame(frame, null);
+
+            // Then
+            Assert.Throws<ArgumentNullException>(() => action());
+        }
+
+        [Test]
+        public void WriteTrackingFrame()
+        {
+            // given
+            var poses = new List<SkeletonPose>()
+            {
+                new SkeletonPose(new PoseDto() { index=0, bones=new() { new() { boneType=BoneType.Chest } }, boneAnchor=new()}, true),
+                new SkeletonPose(new PoseDto() { index=1, bones=new() { new() { boneType=BoneType.Chest } }, boneAnchor=new()}, false),
+            };
+
+            poseSubskeleton.StartPose(poses, true);
+
+            UserTrackingFrameDto frame = new();
+
+            // when
+            poseSubskeleton.WriteTrackingFrame(frame, null);
+
+            // then
+            Assert.AreEqual(poses.Where(x => x.IsCustom).Count(), frame.customPosesIndexes.Count);
+            Assert.AreEqual(poses.Where(x => !x.IsCustom).Count(), frame.environmentPosesIndexes.Count);
+        }
+
+        #endregion WriteTrackingFrame
     }
 }
