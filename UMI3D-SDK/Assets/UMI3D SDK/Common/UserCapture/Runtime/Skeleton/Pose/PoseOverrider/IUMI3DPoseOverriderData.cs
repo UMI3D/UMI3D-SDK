bb723b73db6167c22058e5c0646a07786957e646
CMarichal <charles-eole.marichal@inetum.com>
2023-09-14 17:54:40 +0200

    Add interface for PoseOverrider data
    Decouple from scriptable object

---
+++ UMI3D-SDK/Assets/UMI3D SDK/Common/UserCapture/Runtime/Skeleton/Pose/PoseOverrider/IUMI3DPoseOverriderData.cs
@@ -0,0 +1,31 @@
+﻿/*
+Copyright 2019 - 2023 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System.Collections.Generic;
+
+namespace umi3d.common.userCapture.pose
+{
+    public interface IUMI3DPoseOverriderData
+    {
+        /// <summary>
+        /// The different condition that are needed for the overrider to get activated
+        /// </summary>
+        IReadOnlyList<AbstractPoseConditionDto> PoseConditions { get; }
+        IUMI3DPoseData Pose { get; }
+
+        PoseOverriderDto ToDto(int poseIndexInPoseManager);
+    }
+}
