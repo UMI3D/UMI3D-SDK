dc3b0ce49b7818747321be6b6204bffd0b40f6bc
Romain Tirbisch <romain.tirbisch@inetum.com>
2023-11-13 10:14:46 +0100

    WIP: save system.

--- UMI3D-SDK/Assets/UMI3D SDK/Dependencies/Runtime/Inetum-Unity-Utils/Path.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/Dependencies/Runtime/Inetum-Unity-Utils/Path.cs
@@ -14,30 +14,62 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+using System;
+
 namespace inetum.unityUtils
 {
     public class Path
     {
-
+        /// <summary>
+        /// Combine two paths but instead of throwing an exception when one path is null or empty it returns the other.
+        /// </summary>
+        /// <param name="path1"></param>
+        /// <param name="path2"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <returns></returns>
         public static string Combine(string path1, string path2)
         {
-            char[] charsToTrim = { System.IO.Path.DirectorySeparatorChar, System.IO.Path.AltDirectorySeparatorChar };
-            if (path1 == null || path1 == "") return path2;
-            else if (path2 == null) return path1;
-            else return path1.Trim().TrimEnd(charsToTrim)
-               + System.IO.Path.AltDirectorySeparatorChar
-               + path2.Trim().TrimStart(charsToTrim);
+            char[] charsToTrim = 
+            { 
+                System.IO.Path.DirectorySeparatorChar, 
+                System.IO.Path.AltDirectorySeparatorChar 
+            };
+
+            var path1Trimmed = path1.Trim().TrimEnd(charsToTrim);
+            var path2Trimmed = path2.Trim().TrimStart(charsToTrim);
+
+            if (string.IsNullOrEmpty(path1Trimmed) && string.IsNullOrEmpty(path2Trimmed))
+            {
+                throw new ArgumentNullException($"{nameof(path1)} and {nameof(path2)}", $"Try to combine to empty paths.");
+            }
+            if (string.IsNullOrEmpty(path1Trimmed))
+            {
+                return path2;
+            }
+            else if (string.IsNullOrEmpty(path2Trimmed))
+            {
+                return path1;
+            }
+            else
+            {
+                return System.IO.Path.Combine(path1Trimmed, path2Trimmed);
+            }
         }
 
         public static string Combine(params string[] paths)
         {
-            if (paths == null || paths.Length < 1 || paths[0] == null)
+            if (paths == null || paths.Length == 0)
+            {
                 return null;
+            }
+
             string result = paths[0];
+
             for (int i = 1; i < paths.Length; i++)
             {
                 result = Combine(result, paths[i]);
             }
+
             return result;
         }
     }
