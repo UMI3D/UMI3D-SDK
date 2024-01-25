7c20ff06fa2a9649b408a16176018f630c7fe953
Charles Marichal <charles-eole.marichal@inetum.com>
2024-01-25 17:06:11 +0100

    Use DI in AbstractSkeleton

--- UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/UserCapture/Runtime/Skeleton/AbstractSkeleton.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/UserCapture/Runtime/Skeleton/AbstractSkeleton.cs
@@ -106,8 +106,21 @@ namespace umi3d.cdk.userCapture
 
         #endregion Fields
 
+        #region DI
+
+        protected IUnityMainThreadDispatcher UnityMainThreadDispatcherService { get; private set; }
+
+        public void Init(ITrackedSubskeleton trackedSkeleton, IPoseSubskeleton poseSkeleton, IUnityMainThreadDispatcher mainThreadDispatcher)
+        {
+            UnityMainThreadDispatcherService = mainThreadDispatcher;
+            Init(trackedSkeleton, poseSkeleton);
+        }
+
+        #endregion DI
+
         public void Init(ITrackedSubskeleton trackedSkeleton, IPoseSubskeleton poseSkeleton)
         {
+            UnityMainThreadDispatcherService ??= UnityMainThreadDispatcherManager.Instance;
             this._trackedSkeleton = trackedSkeleton;
             HipsAnchor = TrackedSubskeleton.Hips;
             PoseSubskeleton = poseSkeleton;
@@ -126,8 +139,11 @@ namespace umi3d.cdk.userCapture
             Bones[BoneType.Hips].Position = HipsAnchor != null ? HipsAnchor.position : Vector3.zero;
             Bones[BoneType.Hips].Rotation = HipsAnchor != null ? HipsAnchor.rotation : Quaternion.identity;
 
+            // wait to add PoseSubskeleton until we received at least one frame
             StartCoroutine(InitPoseSubskeleton());
         }
+
+
 
         private IEnumerator InitPoseSubskeleton()
         {
@@ -271,7 +287,7 @@ namespace umi3d.cdk.userCapture
 
             lock (SubskeletonsLock) // loader can start parallel async tasks, required to load concurrently
             {
-                UnityMainThreadDispatcherManager.Instance.Enqueue(() => { lock (SubskeletonsLock) { subskeletons.AddSorted(animatedSubskeleton); } });
+                UnityMainThreadDispatcherService.Enqueue(() => { lock (SubskeletonsLock) { subskeletons.AddSorted(animatedSubskeleton); } });
 
                 // if some animator parameters should be updated by the browsers itself, start listening to them
                 if (animatedSubskeleton.SelfUpdatedAnimatorParameters.Count > 0)
