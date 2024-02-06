d3b61bfe371bf6072b79d056ed7175f8655b1315
Charles Marichal <charles-eole.marichal@inetum.com>
2024-02-06 15:43:19 +0100

    Fix nullref when leaving scene while checking magnitude condition

--- UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/UserCapture/Runtime/Skeleton/Pose/Objects/PoseCondition/MagnitudePoseCondition.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/UserCapture/Runtime/Skeleton/Pose/Objects/PoseCondition/MagnitudePoseCondition.cs
@@ -16,6 +16,7 @@ limitations under the License.
 
 using System;
 using umi3d.cdk.userCapture.tracking;
+using umi3d.common;
 using umi3d.common.userCapture.pose;
 using UnityEngine;
 
@@ -45,6 +46,12 @@ namespace umi3d.cdk.userCapture.pose
         /// <inheritdoc/>
         public bool Check()
         {
+            if (nodeTransform == null) // happens when node is destroyed but condition is not removed.
+            {
+                UMI3DLogger.LogWarning($"Magnitude Pose Condition broke. Node {magnitudeConditionDto.TargetNodeId} destroyed without removing condition first.", DebugScope.CDK | DebugScope.UserCapture);
+                return false;
+            }
+
             Vector3 targetPosition = nodeTransform.position;
 
             Vector3 bonePosition = Vector3.zero;
