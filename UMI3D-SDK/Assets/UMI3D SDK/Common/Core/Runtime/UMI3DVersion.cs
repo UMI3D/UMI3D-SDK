69b76af91e49a58571835485e3369f6e3aa322b1
TheoDesbonnets <56262448+TheoDesbonnets@users.noreply.github.com>
2024-04-02 10:33:59 +0200

    SDK 2.8.r.240402

--- UMI3D-SDK/Assets/UMI3D SDK/Common/Core/Runtime/UMI3DVersion.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/Common/Core/Runtime/UMI3DVersion.cs
@@ -30,8 +30,8 @@ namespace umi3d
         public static string version => major + "." + minor + "." + status + "." + date;
         public static readonly string major = "2";
         public static readonly string minor = "8";
-        public static readonly string status = "p";
-        public static readonly string date = "240326";
+        public static readonly string status = "r";
+        public static readonly string date = "240402";
 
 
         public class Version
