f32178842ca60d44d378ae92112c8bcd3036c4b6
Charles Marichal <charles-eole.marichal@inetum.com>
2024-05-21 16:36:20 +0200

    Add Transformation interface
    - PureTrasnformation -> (position, rotation, scale) container with no notion of aut-computed hierarchy (engineless)
    - UnityTransformation -> Wrapper for Unity Transform, thta possesses a notion of hierarchy

---
+++ UMI3D-SDK/Assets/UMI3D SDK/Common/Core/Runtime/Utils/ITransformation.cs
@@ -0,0 +1,96 @@
+﻿/*
+Copyright 2019 - 2024 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+using UnityEngine;
+
+namespace umi3d.common.core
+{
+    public interface ITransformation
+    {
+        /// <summary>
+        /// World position (global space).
+        /// </summary>
+        public Vector3 Position { get; set; }
+
+        /// <summary>
+        /// World rotation (global space).
+        /// </summary>
+        public Quaternion Rotation { get; set; }
+
+        /// <summary>
+        /// Rotation relative to parent.
+        /// </summary>
+        public Quaternion LocalRotation { get; set; }
+
+        /// <summary>
+        /// Scale relative to parent.
+        /// </summary>
+        public Vector3 Scale { get; set; }
+    }
+
+    /// <summary>
+    /// Transformation container without a gameobject.
+    /// </summary>
+    /// Storage only. No logic is ensured.
+    public class PureTransformation : ITransformation
+    {
+        public Vector3 Position { get; set; } = Vector3.zero;
+
+        public Quaternion Rotation { get; set; } = Quaternion.identity;
+
+        public Quaternion LocalRotation { get; set; } = Quaternion.identity;
+
+        public Vector3 Scale { get; set; } = Vector3.one;
+    }
+
+    /// <summary>
+    /// Transformation wrapper for a Unity tranform.
+    /// </summary>
+    public class UnityTransformation : ITransformation
+    {
+        public Vector3 Position
+        {
+            get => Transform.position;
+            set => Transform.position = value;
+        }
+
+        public Quaternion Rotation
+        {
+            get => Transform.rotation;
+            set => Transform.rotation = value;
+        }
+
+        public Quaternion LocalRotation
+        {
+            get => Transform.localRotation;
+            set => Transform.localRotation = value;
+        }
+
+        public Vector3 Scale
+        {
+            get => Transform.localScale;
+            set => Transform.localScale = value;
+        }
+
+        public Transform Transform { get; }
+
+        public UnityTransformation(Transform transform)
+        {
+            Transform = transform;
+        }
+    }
+}
