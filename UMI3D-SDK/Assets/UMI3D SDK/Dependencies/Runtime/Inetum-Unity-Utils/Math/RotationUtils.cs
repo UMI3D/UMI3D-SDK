111039c0195745013efd6715a140ec963176ce00
Charles Marichal <charles-eole.marichal@inetum.com>
2024-06-19 17:22:03 +0200

    Create RotationUtils
    Centralize methods usefuls for space computing in utils

---
+++ UMI3D-SDK/Assets/UMI3D SDK/Dependencies/Runtime/Inetum-Unity-Utils/Math/RotationUtils.cs
@@ -0,0 +1,41 @@
+﻿/*
+Copyright 2019 - 2024 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using UnityEngine;
+
+namespace inetum.unityUtils.math
+{
+    public static class RotationUtils
+    {
+        /// <summary>
+        /// Project an angle in degrees to [-180°;180°[ range.
+        /// </summary>
+        /// <param name="angle">Angle in degrees</param>
+        /// <returns></returns>
+        public static float ProjectAngleIn180Range(float angle)
+        {
+            return (angle % 360f) < 180f ? angle % 360f : ((angle % 360f) - 360f);
+        }
+
+        public static void ToYawPitchRoll(Quaternion rotation, out float yawAngle, out float pitchAngle, out float rollAngle)
+        {
+            rotation.ToAngleAxis(out rollAngle, out Vector3 zAxis);
+            yawAngle = Vector3.Angle(Vector3.forward, Vector3.ProjectOnPlane(zAxis, Vector3.up));
+            pitchAngle = Vector3.Angle(zAxis, Vector3.ProjectOnPlane(zAxis, Vector3.up));
+        }
+
+    }
+}
