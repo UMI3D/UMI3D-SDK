1cac86457af12eb1698b9644f6a13a6d0174589a
Charles Marichal <charles-eole.marichal@inetum.com>
2024-06-20 09:26:37 +0200

    Introduce Spine Flexer
    Turn the spine when a bone constraint exists

---
+++ UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/UserCapture/Runtime/Skeleton/Tracking/TrackerSimulation/Objects/Flexers/IFlexer.cs
@@ -0,0 +1,39 @@
+﻿/*
+Copyright 2019 - 2024 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+namespace umi3d.cdk.userCapture.tracking.constraint
+{
+    /// <summary>
+    /// Flex a limb based on constraints and controllers.
+    /// </summary>
+    public interface IFlexer
+    {
+        /// <summary>
+        /// Initialize and active the flexer.
+        /// </summary>
+        void Enable();
+
+        /// <summary>
+        /// Reset and deactive the flexer.
+        /// </summary>
+        void Disable();
+
+        /// <summary>
+        /// Bend a limb based on constraints.
+        /// </summary>
+        void Flex();
+    }
+}
