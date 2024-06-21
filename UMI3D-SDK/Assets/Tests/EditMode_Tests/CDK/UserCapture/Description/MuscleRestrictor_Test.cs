5d071980ad8075a57cf02b54b5106f679082b753
Charles Marichal <charles-eole.marichal@inetum.com>
2024-06-21 14:46:27 +0200

    Introduce MuscleRestrictor
    Take care of muscle restriction and applies hysteresis based on previous rotations.

---
+++ UMI3D-SDK/Assets/Tests/EditMode_Tests/CDK/UserCapture/Description/MuscleRestrictor_Test.cs
@@ -0,0 +1,119 @@
+﻿/*
+Copyright 2019 - 2024 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using NUnit.Framework;
+using TestUtils;
+using umi3d.cdk.userCapture.description;
+using umi3d.common.userCapture;
+using umi3d.common.userCapture.description;
+using UnityEngine;
+
+namespace EditMode_Tests.UserCapture.Description.CDK
+{
+    [TestFixture, TestOf(typeof(MuscleRestrictor))]
+    public class MuscleRestrictor_Test
+    {
+        #region Restrict
+
+        private static readonly Quaternion[] restrictTestValues = new Quaternion[]
+        {
+            Quaternion.identity,
+            Quaternion.Euler(0, 0, 0),
+            Quaternion.Euler(0, 5, 0),
+            Quaternion.Euler(5, 5, 5),
+            Quaternion.Euler(0, -5, 0),
+            Quaternion.Euler(-5, -5, -5),
+            Quaternion.Euler(0, -10, 0),
+            Quaternion.Euler(0, 10, 0),
+        };
+
+        [Test, TestOf(nameof(MuscleRestrictor.Restrict))]
+        public void Restrict([ValueSource(nameof(restrictTestValues))] Quaternion value)
+        {
+            IUMI3DSkeletonMusclesDefinition.Muscle muscle = new()
+            {
+                Bonetype = BoneType.Chest,
+                ReferenceFrameRotation = Quaternion.identity.Dto(),
+                XRotationRestriction = new()
+                {
+                    min = -2,
+                    max = 2
+                },
+                YRotationRestriction = new()
+                {
+                    min = -2,
+                    max = 2
+                },
+                ZRotationRestriction = new()
+                {
+                    min = -2,
+                    max = 2
+                }
+            };
+
+            // given
+            MuscleRestrictor muscleRestrictor = new MuscleRestrictor(muscle);
+            Quaternion muscleLocalRotation = value;
+
+            // when
+            Quaternion muscleLocalRotationRestricted = muscleRestrictor.Restrict(muscleLocalRotation);
+
+            // then
+            Vector3 muscleLocalRotationEulersInRange = inetum.unityUtils.math.RotationUtils.ProjectAngleIn180Range(muscleLocalRotation.eulerAngles);
+            AssertUnityStruct.AreRotationsEqual(Quaternion.Euler(Mathf.Clamp(muscleLocalRotationEulersInRange.x, muscle.XRotationRestriction.Value.min, muscle.XRotationRestriction.Value.max),
+                                                                Mathf.Clamp(muscleLocalRotationEulersInRange.y, muscle.YRotationRestriction.Value.min, muscle.YRotationRestriction.Value.max),
+                                                                Mathf.Clamp(muscleLocalRotationEulersInRange.z, muscle.ZRotationRestriction.Value.min, muscle.ZRotationRestriction.Value.max)),
+                                                                muscleLocalRotationRestricted);
+        }
+
+        [Test]
+        public void Restrict_NoRestriction([ValueSource(nameof(restrictTestValues))] Quaternion value)
+        {
+            IUMI3DSkeletonMusclesDefinition.Muscle muscle = new()
+            {
+                Bonetype = BoneType.Chest,
+                ReferenceFrameRotation = Quaternion.identity.Dto(),
+                XRotationRestriction = new()
+                {
+                    min = -10,
+                    max = 10
+                },
+                YRotationRestriction = new()
+                {
+                    min = -10,
+                    max = 10
+                },
+                ZRotationRestriction = new()
+                {
+                    min = -10,
+                    max = 10
+                }
+            };
+
+            // given
+            MuscleRestrictor muscleRestrictor = new MuscleRestrictor(muscle);
+            Quaternion muscleLocalRotation = value;
+
+            // when
+            Quaternion muscleLocalRotationRestricted = muscleRestrictor.Restrict(muscleLocalRotation);
+
+            // then
+            AssertUnityStruct.AreEqual(muscleLocalRotation, muscleLocalRotationRestricted);
+        }
+
+        #endregion Restrict
+    }
+}
---
+++ UMI3D-SDK/Assets/UMI3D SDK/ClientDevelopmentKit/UserCapture/Runtime/Skeleton/Description/MuscleRestrictor.cs
@@ -0,0 +1,73 @@
+﻿/*
+Copyright 2019 - 2024 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using umi3d.common.userCapture.description;
+
+using UnityEngine;
+
+namespace umi3d.cdk.userCapture.description
+{
+    /// <summary>
+    /// Controls the restriction applied by a muscle
+    /// </summary>
+    public class MuscleRestrictor
+    {
+        private IUMI3DSkeletonMusclesDefinition.Muscle muscleDefinition;
+
+        private float lastAcceptableXAngle = 0f;
+        private float lastAcceptableYAngle = 0f;
+        private float lastAcceptableZAngle = 0f;
+
+        public MuscleRestrictor(IUMI3DSkeletonMusclesDefinition.Muscle muscle)
+        {
+            this.muscleDefinition = muscle;
+        }
+
+        public Quaternion Restrict(Quaternion boneLocalRotation)
+        {
+            // compare orientations
+            Quaternion referenceFrame = muscleDefinition.ReferenceFrameRotation.Quaternion();
+            Vector3 rotation = (referenceFrame * boneLocalRotation).eulerAngles;
+
+            float xAngle = inetum.unityUtils.math.RotationUtils.ProjectAngleIn180Range(rotation.x);
+            float yAngle = inetum.unityUtils.math.RotationUtils.ProjectAngleIn180Range(rotation.y);
+            float zAngle = inetum.unityUtils.math.RotationUtils.ProjectAngleIn180Range(rotation.z);
+
+            // if restricted, appply restriction, do not move unless a new acceptable value is reached
+            static float ClampHysteresis(float angle, ref float lastAcceptableAngle, IUMI3DSkeletonMusclesDefinition.Muscle.RotationRestriction restriction)
+            {
+                if (restriction.min <= angle && angle <= restriction.max)
+                {
+                    lastAcceptableAngle = angle;
+                    return angle;
+                }
+                else
+                {
+                    if (lastAcceptableAngle != 0f)
+                        return (lastAcceptableAngle <= (restriction.min + restriction.max) / 2) ? restriction.min : restriction.max;
+                    else
+                        return Mathf.Clamp(angle, restriction.min, restriction.max);
+                }
+            }
+
+            float xAngleRestricted = muscleDefinition.XRotationRestriction.HasValue ? ClampHysteresis(xAngle, ref lastAcceptableXAngle, muscleDefinition.XRotationRestriction.Value) : xAngle;
+            float yAngleRestricted = muscleDefinition.YRotationRestriction.HasValue ? ClampHysteresis(yAngle, ref lastAcceptableYAngle, muscleDefinition.YRotationRestriction.Value) : yAngle;
+            float zAngleRestricted = muscleDefinition.ZRotationRestriction.HasValue ? ClampHysteresis(zAngle, ref lastAcceptableZAngle, muscleDefinition.ZRotationRestriction.Value) : zAngle;
+
+            return Quaternion.Inverse(referenceFrame) * Quaternion.Euler(xAngleRestricted, yAngleRestricted, zAngleRestricted);
+        }
+    }
+}
