3dc5f83ea4292a02cb0257a0ffccf8d8aa7bf428
TheoDesbonnets <56262448+TheoDesbonnets@users.noreply.github.com>
2024-09-24 17:56:42 +0200

    remove using

--- UMI3D-SDK/Assets/UMI3D SDK/Common/Collaboration/Runtime/Dto/Signaling/UserDto.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/Common/Collaboration/Runtime/Dto/Signaling/UserDto.cs
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-using Codice.Client.Common;
 using System;
 using System.Collections.Generic;
 
