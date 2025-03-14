3e83f51173a5a109078f66edaa7ecb2abb5a0a79
TheoDesbonnets <56262448+TheoDesbonnets@users.noreply.github.com>
2025-03-14 10:55:18 +0100

    Split files

--- UMI3D-SDK/Assets/UMI3D SDK/Common/Core/Runtime/Serializer/UMI3DSerializerBasicModules.cs
+++ UMI3D-SDK/Assets/UMI3D SDK/Common/Core/Runtime/Serializer/UMI3DSerializerBasicModules.cs
@@ -15,34 +15,9 @@ limitations under the License.
 */
 
 using System;
-using System.Collections;
-using umi3d.common;
-using UnityEngine;
 
 namespace umi3d.common
 {
-    public class HeaderContentSerializer : UMI3DSerializerModule<HeaderContent>
-    {
-        public bool IsCountable()
-        {
-            return true;
-        }
-
-        public bool Read(ByteContainer container, out bool readable, out HeaderContent result)
-        {
-            readable = UMI3DSerializer.TryRead(container, out string header);
-            readable &= UMI3DSerializer.TryRead(container, out string content);
-
-            result = readable ? new HeaderContent() { header = header, content = content } : null;
-            return readable;
-        }
-
-        public bool Write(HeaderContent value, out Bytable bytable, params object[] parameters)
-        {
-            bytable = UMI3DSerializer.Write(value.header) + UMI3DSerializer.Write(value.content);
-            return true;
-        }
-    }
 
     [UMI3DSerializerOrder(1000)]
     public class UMI3DSerializerBasicModules : UMI3DSerializerModule
